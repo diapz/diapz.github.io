3e31439c14ddd7fb56ad92259f543db728ddaa28
diapz <gdiapana_230000001047@uic.edu.ph>
2024-10-02 11:46:01 +0900

    V5.2
    updated

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 const roleElement = document.getElementById('role');
-const roles = ['Geoffrey Diapana'];
+const roles = ['Geoffrey Diapana','Geoffrey "Fr3y" Diapana'];
 let roleIndex = 0;
 let charIndex = 0;
 let deleting = false;
 let typingSpeed = 150; 
 let deletingSpeed = 200; 
-let pauseBeforeDeleting = 2200; 
-let pauseAfterDeleting = 550; 
+let pauseBeforeDeleting = 1200; 
+let pauseAfterDeleting = 3550; 
 
 function type() {
     if (deleting) {
-        if (charIndex > 1) {
+        if (charIndex > 8) {
             roleElement.textContent = roles[roleIndex].substring(0, charIndex - 1);
             charIndex--;
             setTimeout(type, deletingSpeed);
