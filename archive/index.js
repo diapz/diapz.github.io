fe6188f3af00241580266a015fe0aabe7a51190d
diapz <gdiapana_230000001047@uic.edu.ph>
2024-10-16 01:35:59 +0800

    V6.1
    With Light Mode and Dark Mode :))

--- archive/index.js
+++ archive/index.js
@@ -1,38 +1,3 @@
-const roleElement = document.getElementById('role');
-const roles = ['Geoffrey Diapana','Geoffrey "Fr3y" Diapana'];
-let roleIndex = 0;
-let charIndex = 0;
-let deleting = false;
-let typingSpeed = 150; 
-let deletingSpeed = 200; 
-let pauseBeforeDeleting = 1200; 
-let pauseAfterDeleting = 3550; 
-
-function type() {
-    if (deleting) {
-        if (charIndex > 8) {
-            roleElement.textContent = roles[roleIndex].substring(0, charIndex - 1);
-            charIndex--;
-            setTimeout(type, deletingSpeed);
-        } else {
-            deleting = false;
-            roleIndex = (roleIndex + 1) % roles.length;
-            setTimeout(type, pauseBeforeDeleting);
-        }
-    } else {
-        if (charIndex < roles[roleIndex].length) {
-            roleElement.textContent = roles[roleIndex].substring(0, charIndex + 1);
-            charIndex++;
-            setTimeout(type, typingSpeed);
-        } else {
-            deleting = true;
-            setTimeout(type, pauseAfterDeleting); 
-        }
-    }
-}
-
-type();
-
 const darkModeMediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
 
 let darkMode = darkModeMediaQuery.matches;
