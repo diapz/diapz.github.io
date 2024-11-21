00942b2b4e2a0805efdc75c129f22640488c2cb7
diapz <gdiapana_230000001047@uic.edu.ph>
2024-11-21 23:34:23 +0800

    v8.2

--- script.js
+++ script.js
@@ -93,38 +93,3 @@ navbarToggle.addEventListener('click',  
   navbarMenu.classList.toggle('active');   
 
 });
-
-
-
-//FOR API PART
-
-function greet(name)
-{
-    console.log(`Hello ${name}`);
-
-    //document.getElementById("greet").innerHTML=`Hello ${name}`;
-    document.querySelector(".greet").innerHTML=`Hello ${name}`;
-}
-
-function callAPI(){
-    fetch('https://jsonplaceholder.typicode.com/todos/2')
-      .then(response => response.json())
-      .then(json => {
-        console.log(json);
-        document.querySelector("#todo1_id").innerHTML = json.id;
-        document.querySelector("#todo1_title").innerHTML = json.title;
-        document.querySelector("#todo1_completed").innerHTML = json.completed;
-      });
-}
-
-greet("Clyde"); //Hello Clyde
-callAPI();
-
-/*
-function greet(name)
-{
-    return `Hello ${name}`;
-}s
-
-console.log(greet("Clyde")); //Hello Clyde
-*/
